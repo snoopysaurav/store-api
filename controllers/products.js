6dd16c91f12d8c0f32f77afed587dcac92550a45
Saurav Khatiwada <saurav2k60@gmail.com>
2022-12-19 21:27:13 +0545

    ✨ Sorting using regex.

--- controllers/products.js
+++ controllers/products.js
@@ -1,13 +1,22 @@
 import { Product } from "../models/products.js";
+
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "vase table" });
+  const products = await Product.find({ name: "2" });
   res.status(200).json({ products, nbHits: products.length });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({
-    msg: "Products route.",
-  });
+const getAllProducts = async (req, res) => {
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 export { getAllProductsStatic, getAllProducts };
