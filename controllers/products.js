c7aa062a5ad6a49e58a7961556ee9e45f4ac8ca5
Saurav Khatiwada <saurav2k60@gmail.com>
2022-12-22 23:58:46 +0545

    ✨ Sort Updated

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 import { Product } from "../models/products.js";
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ name: "2" });
+  const products = await Product.find({}).select("name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -14,8 +14,19 @@ const getAllProducts = async (req, res) => {
   if (company) {
     queryObject.company = company;
   }
-  console.log(queryObject);
-  const products = await Product.find(queryObject);
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+    // products = products.sort();
+  } else {
+    result = result.sort("createAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
